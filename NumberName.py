dcc9b0681365c88d21f4dba1d660c489465e90fb
bigbadbuff <31122423+bigbadbuff@users.noreply.github.com>
2018-05-15 09:25:29 -0400

    Update NumberName.py
    Given the input of a user's name, print the user's name with one random character within the name on the line's above and below the name.

--- NumberName.py
+++ NumberName.py
@@ -1,14 +1,20 @@
-import random
+#Assignment:
+#Given the input of a user's name, print the user's name with a random character
+#from the name on the lines above and below the reprinted name.
 
-def randomChar(inName):
+
+import random  #We need this for the to select the character from the name
+
+def randomChar(inName):    
     userNameLen = len(inName)
-    charPos = random.randint(1,userNameLen)
-    print (str(userName[charPos]))
+    charPos = random.randint(1,userNameLen) #This function selects a random integer from 1-x where x is the length of the user's name
+    print (str(userName[charPos])) #This line returns the character corresponding to the randomly selected
+				   #position and prints the character on the line above the name
     return userName[charPos]
 	
-userName = input("type your first name")
+userName = input("type your first name")   #User inputs their name here
 
 print("\n")
-print(userName + "\n" + str(randomChar(userName)))
+print(userName + "\n" + str(randomChar(userName))) #Print function only prints name and character below it
 
-exiter = input("Press any key to continue...")
+exiter = input("Press any key to exit...")
